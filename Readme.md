06c87c3c49755b1c40870284c9a871af90a84df8
sohamganatra <sohamganatra1@gmail.com>
2015-12-10 10:00:46 +0530

    My first commit.

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is code for my chess Engine. I am writing it in c++.
---
+++ init.cpp
@@ -0,0 +1,12 @@
+# include "def.h"
+
+int Sq120ToSq64[BRD_SQ_NUM];
+int Sq64ToSq120[64];
+
+void InitSq120ToSq64(){
+	int index=0;
+	int file=File_A;
+	int rank=Rank_1;
+	int sq=A1;
+	int sq64=0;
+}
